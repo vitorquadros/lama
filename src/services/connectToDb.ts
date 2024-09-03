11779ca443abc898f7496347ff44efd62b9e6fda
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-03 20:58:43 -0300

    feat (db): create mongodb connection

---
+++ src/services/connectToDb.ts
@@ -0,0 +1,23 @@
+import mongoose, { type ConnectionStates } from 'mongoose';
+
+interface Connection {
+  isConnected: ConnectionStates;
+}
+
+const connection: Connection = {
+  isConnected: 0
+};
+
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log('Using existing connection');
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
