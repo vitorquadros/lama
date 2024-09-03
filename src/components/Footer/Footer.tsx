803a60525647b4a923b12453c37a418faef8a623
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-03 12:44:41 -0300

    feat (Footer): information

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,12 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>Lama creative thoughts agency © All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
