3bf7af4b3359e4affa7f7f2a5bdfcfe2b93fab46
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 14:22:19 -0300

    feat (Login): login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
-import { handleGithubLogin } from '@/actions/authActions';
+import { handleGithubLogin, login } from '@/actions/authActions';
 
 const LoginPage = async () => {
   return (
     <div>
       <form action={handleGithubLogin}>
         <button type="submit">Login with Github</button>
+      </form>
+      <form action={login}>
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login with credentials</button>
       </form>
     </div>
   );
