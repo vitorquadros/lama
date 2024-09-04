e914ed71480c2d5ca44fb4f7e6b03b1082e70007
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 13:53:57 -0300

    feat (Register): create and style form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,20 @@
+import { register } from '@/actions/authActions';
+import styles from './register.module.css';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={register} className={styles.form}>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="text" name="email" placeholder="Email" />
+          <input type="text" name="password" placeholder="Password" />
+          <input type="text" name="passwordConfirm" placeholder="Password confirmation" />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
