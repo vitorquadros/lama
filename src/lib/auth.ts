92e3d118e5549c2ba8d5140caf4ddecb5ca4c179
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 14:21:42 -0300

    feat (auth provider): credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,32 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { connectToDb } from './utils';
 import { User } from './models';
+import bcrypt from 'bcrypt';
+
+const login = async (credentials: { username: string; password: string }) => {
+  try {
+    connectToDb();
+    const user = await User.findOne({ username: credentials.username });
+
+    if (!user) {
+      throw new Error('User not found!');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new Error('Wrong credentials!');
+    }
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to login!');
+  }
+};
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -9,7 +34,21 @@ export const {
   signOut
 } = NextAuth({
   providers: [
-    GitHub({ clientId: process.env.GITHUB_ID || '', clientSecret: process.env.GITHUB_SECRET || '' })
+    GitHub({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || ''
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials as { username: string; password: string });
+          return user;
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      }
+    })
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
