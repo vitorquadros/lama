10b5e3de99efab67234b9ba0343f5bacf527a653
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 12:13:30 -0300

    feat (auth actions): logout

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,7 +1,11 @@
 'use server';
 
-import { signIn } from '@/lib/auth';
+import { signIn, signOut } from '@/lib/auth';
 
 export const handleGithubLogin = async () => {
   await signIn('github');
+};
+
+export const handleLogout = async () => {
+  await signOut();
 };
