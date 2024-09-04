53652e770049dc43f40fa6c1b127f5f59958bd43
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 13:43:29 -0300

    feat (auth): create new user on db when log in with github

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { connectToDb } from './utils';
+import { User } from './models';
 export const {
   handlers: { GET, POST },
   auth,
@@ -8,5 +10,30 @@ export const {
 } = NextAuth({
   providers: [
     GitHub({ clientId: process.env.GITHUB_ID || '', clientSecret: process.env.GITHUB_SECRET || '' })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(user, account, profile);
+      if (account?.provider === 'github') {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile?.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile?.login,
+              email: profile?.email,
+              image: profile?.avatar_url
+            });
+
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return true;
+    }
+  }
 });
