72b49105cab2f860eaf3b4f8dc9924937183e102
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-03 21:54:36 -0300

    feat (PostUser): display post user

--- src/components/PostUser/PostUser.tsx
+++ src/components/PostUser/PostUser.tsx
@@ -1,10 +1,34 @@
+import { getUser } from '@/lib/services';
 import styles from './PostUser.module.css';
+import type { User } from '@/@types/user';
+import Image from 'next/image';
 
-const PostUser = () => {
+interface Props {
+  userId: string;
+}
+
+const PostUser = async (props: Props) => {
+  const { userId } = props;
+
+  const user: User = await getUser(userId);
+
+  if (!user) {
+    return <></>;
+  }
+
   return (
     <div className={styles.container}>
-      <span className={styles.detailTitle}>Author</span>
-      <span className={styles.detailValue}>Vitor Quadros</span>
+      <Image
+        src={user.img ?? '/noavatar.png'}
+        alt="Avatar Image"
+        width={50}
+        height={50}
+        className={styles.avatarImg}
+      />
+      <div className={styles.texts}>
+        <span className={styles.detailTitle}>Author</span>
+        <span className={styles.detailValue}>{user.username}</span>
+      </div>
     </div>
   );
 };
