6ff94edba5c4c5dee7d09adc2a5ea7160705d37d
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 11:21:38 -0300

    feat (PostCard): add data from posts

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -17,7 +17,7 @@ const PostCard = (props: Props) => {
           <Image src={post.img ?? '/noimage.png'} alt="Post" fill className={styles.img} />
         </div>
 
-        <span className={styles.date}>01/01/2024</span>
+        <span className={styles.date}>{post.createdAt.toString().slice(4, 16)}</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>{post.title}</h1>
