607f9910c6d352657b6900761c91bacd7d6278d6
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 13:49:47 -0300

    feat (auth actions): register with email and password

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { signIn, signOut } from '@/lib/auth';
+import { User } from '@/lib/models';
+import { connectToDb } from '@/lib/utils';
 
 export const handleGithubLogin = async () => {
   await signIn('github');
@@ -8,4 +10,40 @@ export const handleGithubLogin = async () => {
 
 export const handleLogout = async () => {
   await signOut();
+};
+
+export const register = async (formData: FormData) => {
+  const { username, email, password, passwordConfirm, img } = Object.fromEntries(formData) as {
+    username: string;
+    email: string;
+    img: string;
+    password: string;
+    passwordConfirm: string;
+  };
+
+  if (password !== passwordConfirm) {
+    return 'Passwords do not match.';
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return 'Username already exists';
+    }
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img
+    });
+
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    return { error: 'Something went wrong.' };
+  }
 };
