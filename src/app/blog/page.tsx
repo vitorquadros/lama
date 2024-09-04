95fae573aa165441e07f8a7e97a3eef64725fe09
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-03 21:29:43 -0300

    feat (Blog): fetch and display posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,17 @@
 import PostCard from '@/components/PostCard/PostCard';
 import styles from './blog.module.css';
+import { getPosts } from '@/lib/services';
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const posts = await getPosts();
+
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
+      {posts.map((post) => (
+        <div className={styles.post} key={post._id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -2,23 +2,31 @@ import Image from 'next/image';
 import styles from './PostCard.module.css';
 import Link from 'next/link';
 
-const PostCard = () => {
+interface Props {
+  post: {
+    title: string;
+    description: string;
+    img?: string;
+    userId: string;
+  };
+}
+
+const PostCard = (props: Props) => {
+  const { post } = props;
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/27703479/pexels-photo-27703479/free-photo-of-ceu-por-do-sol-feriado-ponto-de-referencia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Post"
-            fill
-            className={styles.img}
-          />
-        </div>
+        {post.img && (
+          <div className={styles.imageContainer}>
+            <Image src={post.img} alt="Post" fill className={styles.img} />
+          </div>
+        )}
         <span className={styles.date}>01/01/2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.description}>Description</p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.description}>{post.description}</p>
         <Link href="/blog/post" className={styles.link}>
           Read more
         </Link>
