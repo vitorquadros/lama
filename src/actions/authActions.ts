1a7d601d63e71183ffbad8efc923d5d3bbe98efa
Vitor Quadros <viitorquadros@hotmail.com>
2024-09-04 14:22:07 -0300

    feat (auth actions): login with credentials

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -13,6 +13,25 @@ export const handleLogout = async () => {
   await signOut();
 };
 
+export const login = async (formData: FormData) => {
+  const { username, password } = Object.fromEntries(formData) as {
+    username: string;
+    password: string;
+  };
+
+  try {
+    connectToDb();
+
+    await signIn('credentials', {
+      username,
+      password
+    });
+  } catch (error) {
+    console.log(error);
+    return { error: 'Something went wrong.' };
+  }
+};
+
 export const register = async (formData: FormData) => {
   const { username, email, password, passwordConfirm, img } = Object.fromEntries(formData) as {
     username: string;
